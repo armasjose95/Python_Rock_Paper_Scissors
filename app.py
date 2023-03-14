67886620f26d17a9d9b341bc652c3b41e3ba6427
Jose Armas <armasjose95@gmail.com>
2023-03-13 23:50:24 -0500

    creating random computer choice

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-# computerChoiceDisplayed =
+import random
+userChoice = input('Enter your choice: ')
+print(userChoice)
+
+
+def generateComputerChoice(randomComputerChoice):
+    computerChoice = ['Rock', 'Paper', 'Scissors']
+    randomComputerChoice = random.choice(computerChoice)
+    print(randomComputerChoice)
+
+
+generateComputerChoice(randomComputerChoice)
+
+
+def result():
+    if randomComputerChoice == userChoice:
+        print('Tie')
+    else:
+        print('Not a tie')
