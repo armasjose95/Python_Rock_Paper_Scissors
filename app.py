7a67a06825f95cc76a056cedb4b30f728240a669
Jose Armas <armasjose95@gmail.com>
2023-03-14 23:36:55 -0500

    reating logic function for the game

--- app.py
+++ app.py
@@ -12,8 +12,21 @@ def generateComputerChoice(randomComputerChoice):
 generateComputerChoice(randomComputerChoice)
 
 
-def result():
+def result(randomComputerChoice):
     if randomComputerChoice == userChoice:
         print('Tie')
-    else:
-        print('Not a tie')
+    elif randomComputerChoice == 'Rock' and userChoice == 'Scissors':
+        print('You lost')
+    elif randomComputerChoice == 'Rock' and userChoice == 'Paper':
+        print('You won!')
+    elif randomComputerChoice == 'Paper' and userChoice == 'Scissors':
+        print('You won!')
+    elif randomComputerChoice == 'Paper' and userChoice == 'Rock':
+        print('You lost')
+    elif randomComputerChoice == 'Scissors' and userChoice == 'Rock':
+        print('You won!')
+    elif randomComputerChoice == 'Scissors' and userChoice == 'Paper':
+        print('You lost')
+
+
+result(randomComputerChoice)
