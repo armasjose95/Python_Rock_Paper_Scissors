836d8f7cf319ddd5ee00066b49c1a1c79f799b6e
Jose Armas <armasjose95@gmail.com>
2023-03-19 18:09:46 -0500

    modifying function to print something new

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ def result():
     if randomComputerChoice == userChoice:
         print('Tie')
     elif randomComputerChoice == 'Rock' and userChoice == 'Scissors':
-        print('You lost')
+        print('You lost! Rock beats Scissors!')
     elif randomComputerChoice == 'Rock' and userChoice == 'Paper':
-        print('You won!')
+        print('You won! Paper beats Rock!')
     elif randomComputerChoice == 'Paper' and userChoice == 'Scissors':
-        print('You won!')
+        print('You won! Scissors beats Paper!')
     elif randomComputerChoice == 'Paper' and userChoice == 'Rock':
-        print('You lost')
+        print('You lost! Paper beats Rock!')
     elif randomComputerChoice == 'Scissors' and userChoice == 'Rock':
-        print('You won!')
+        print('You won! Rock beats Scissors!')
     elif randomComputerChoice == 'Scissors' and userChoice == 'Paper':
-        print('You lost')
+        print('You lost! Scissors beats Paper!')
 
 
 result()
