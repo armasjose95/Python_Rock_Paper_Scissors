c2eaaad7db43cad0bd0541dc011ddb8a1fce0aeb
Jose Armas <armasjose95@gmail.com>
2023-05-07 22:59:19 -0500

    cleaning up code

--- app.py
+++ app.py
@@ -16,18 +16,28 @@ while True:
     def result():
         if randomComputerChoice == userChoice:
             print('Tie')
-        elif randomComputerChoice == 'Rock' and userChoice == 'Scissors':
-            print('You lost! Rock beats Scissors!')
-        elif randomComputerChoice == 'Rock' and userChoice == 'Paper':
-            print('You won! Paper beats Rock!')
-        elif randomComputerChoice == 'Paper' and userChoice == 'Scissors':
-            print('You won! Scissors beats Paper!')
-        elif randomComputerChoice == 'Paper' and userChoice == 'Rock':
-            print('You lost! Paper beats Rock!')
-        elif randomComputerChoice == 'Scissors' and userChoice == 'Rock':
-            print('You won! Rock beats Scissors!')
-        elif randomComputerChoice == 'Scissors' and userChoice == 'Paper':
-            print('You lost! Scissors beats Paper!')
+        # elif randomComputerChoice == 'Rock' and userChoice == 'Scissors':
+            #print('You lost! Rock beats Scissors!')
+        # elif randomComputerChoice == 'Rock' and userChoice == 'Paper':
+            #print('You won! Paper beats Rock!')
+        # elif randomComputerChoice == 'Paper' and userChoice == 'Scissors':
+            #print('You won! Scissors beats Paper!')
+        # elif randomComputerChoice == 'Paper' and userChoice == 'Rock':
+            #print('You lost! Paper beats Rock!')
+        # elif randomComputerChoice == 'Scissors' and userChoice == 'Rock':
+            #print('You won! Rock beats Scissors!')
+        # elif randomComputerChoice == 'Scissors' and userChoice == 'Paper':
+            #print('You lost! Scissors beats Paper!')
+
+        if youWon(userChoice, randomComputerChoice):
+            return "You picked {} and the computer picked {}. You won!".format(userChoice, randomComputerChoice)
+
+        return "You picked {} and the computer picked {}. You lost!".format(userChoice, randomComputerChoice)
+
+          def youWon(userChoice, randomComputerChoice):
+               if (randomComputerChoice == 'Rock' and userChoice) == 'Paper' or (randomComputerChoice == 'Paper' and userChoice == 'Scissors') or (randomComputerChoice == 'Scissors' and userChoice == 'Rock'):
+                    return True
+                return False
 
     result()
 
