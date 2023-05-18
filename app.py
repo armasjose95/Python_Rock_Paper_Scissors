88f303f06d59af601e03eab72f813dca61b95f3b
Jose Armas <armasjose95@gmail.com>
2023-05-17 23:21:06 -0500

    added a best out of 3 wins functin. Score adds up after each victory and the game is fully up and running

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import math
 
 user_score = 0
 computer_score = 0
+# Rounds up to 2, which wins the game. 2/3 wins
+win = math.ceil(3/2)
 
 while True:
     # User instructions
-    print("Choose your weapon of choice: (Rock, Paper or Scissors)")
+    print("Best out of 3 games. \nChoose your weapon of choice: (rock, paper or scissors)")
     # User selects between the three choices
     user_choice = input('Enter your choice: ')
-    available_choices = ['Rock', 'Paper', 'Scissors']
+    available_choices = ['rock', 'paper', 'scissors']
 
     if user_choice in available_choices:
         # Setting how computer selects it's choice randomly
@@ -24,6 +26,7 @@ while True:
         global user_score
         global computer_score
 
+        # Tie/Win/Lose Scenarios of the game
         if random_computer_choice == user_choice:
             print('Tie')
         elif random_computer_choice == 'Rock' and user_choice == 'Paper':
@@ -43,6 +46,13 @@ while True:
             f"\nUser score = {user_score} and Computer Score = {computer_score} ")
 
     result()
+
+    if user_score == win or computer_score == win:
+        if user_score > computer_score:
+            print('You won the series of games! :) ')
+        else:
+            print('You lost the series of games! :(')
+            break
 
     # Option for user to keep playing.
     play_again = input('Want to play again? (y/n): ')
